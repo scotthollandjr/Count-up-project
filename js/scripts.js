74a6a1b157c64ae2de7e0a4a8877edde09ca727c
Scott Holland <scotthollandjr@gmail.com>
2016-02-10 17:33:20 -0800

    Add countUp functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,11 @@
 var countUp = function(a, b) {
-  
+  var multiples = [];
+
+  var a = 30;
+  var b = 5;
+
+  for ( i = b; i < a; i + b ) {
+      multiples.push[i + ","];
   }
+  alert(multiples);
 }
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,5 @@
 describe('countUp', function() {
-  it('count up to "a" by multiples of "b"', function() {
+  it('count up to a by multiples of b', function() {
     expect(countUp(30, 5)).to.equal("5,10,15,20,25,30")
   });
 });
